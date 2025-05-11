5f819235f78ed3ef241f4e2477afdfd972ee91e7
Niko <niko.dansh@gmail.com>
2025-05-11 21:28:07 +0300

    чтото сделал

---
+++ Lesson 11 YAML, JSON, CSV/Lesson 11 DZ.py
@@ -0,0 +1,9 @@
+import yaml
+
+people = [
+    {'name': 'Анна', 'age': 28, 'city': 'Москва'},
+    {'name': 'Иван', 'age': 35, 'city': 'Санкт-Петербург'},
+    {'name': 'Ольга', 'age': 22, 'city': 'Новосибирск'}
+]
+
+with open(r'yaml_people_list_DZ.yaml', 'w')
